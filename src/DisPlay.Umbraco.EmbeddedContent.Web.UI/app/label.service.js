985e82af9f44e103f32ef015c9c7548ce3ec8bd4
Rasmus John Pedersen <rpe@dis-play.dk>
2017-09-08 12:08:19 +0200

    Add label support for MNTP2 and MediaPicker2

--- src/DisPlay.Umbraco.EmbeddedContent.Web.UI/app/label.service.js
+++ src/DisPlay.Umbraco.EmbeddedContent.Web.UI/app/label.service.js
@@ -1,30 +1,36 @@
+const MultiNodeTreePickerResolver = (property, cacheService) => {
+  const ids = property.value.split(',')
+  let type = 'Document'
+
+  switch (property.config.startNode.type) {
+    case 'media':
+      type = 'Media'
+      break
+    case 'member':
+      type = 'Member'
+      break
+  }
+
+  return ids.map(id => cacheService.getEntityById(id, type))
+    .filter(entity => entity)
+    .map(entity => entity.name)
+    .join(', ')
+}
+
+const MediaPickerResolver = (property, cacheService) => {
+  const ids = property.value.split(',')
+
+  return ids.map(id => cacheService.getEntityById(id, 'Media'))
+    .filter(entity => entity)
+    .map(entity => entity.name)
+    .join(', ')
+}
+
 export const resolvers = {
-  'Umbraco.MultipleMediaPicker': (property, cacheService) => {
-    const ids = property.value.split(',')
-
-    return ids.map(id => cacheService.getEntityById(+id, 'Media'))
-      .filter(entity => entity)
-      .map(entity => entity.name)
-      .join(', ')
-  },
-  'Umbraco.MultiNodeTreePicker': (property, cacheService) => {
-    const ids = property.value.split(',')
-    let type = 'Document'
-
-    switch (property.config.startNode.type) {
-      case 'media':
-        type = 'Media'
-        break
-      case 'member':
-        type = 'Member'
-        break
-    }
-
-    return ids.map(id => cacheService.getEntityById(+id, type))
-      .filter(entity => entity)
-      .map(entity => entity.name)
-      .join(', ')
-  },
+  'Umbraco.MultipleMediaPicker': MediaPickerResolver,
+  'Umbraco.MediaPicker2': MediaPickerResolver,
+  'Umbraco.MultiNodeTreePicker': MultiNodeTreePickerResolver,
+  'Umbraco.MultiNodeTreePicker2': MultiNodeTreePickerResolver,
   'Umbraco.TinyMCEv3': (property, cacheService) => String(property.value).replace(/<[^>]+>/gm, ''),
   'RJP.MultiUrlPicker': (property, cacheService) => property.value.map(link => link.name).join(', ')
 }
