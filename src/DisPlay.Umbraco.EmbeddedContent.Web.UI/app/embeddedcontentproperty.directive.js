7181580457c9928806552b56837d098e2bb55d78
Simon Hviid Larsen <sla@dis-play.dk>
2017-12-08 15:34:23 +0100

    Manually update property value instead of two way binding

--- src/DisPlay.Umbraco.EmbeddedContent.Web.UI/app/embeddedcontentproperty.directive.js
+++ src/DisPlay.Umbraco.EmbeddedContent.Web.UI/app/embeddedcontentproperty.directive.js
@@ -1,7 +1,9 @@
+import angular from 'angular'
+
 export default function EmbeddedContentPropertyDirective (fileManager) {
   return {
     restrict: 'E',
-    template: '<umb-property property="property" ng-if="ready"><umb-property-editor model="property"></umb-property-editor></umb-property>',
+    template: '<umb-property property="property" ng-if="ready"><umb-property-editor model="editProperty"></umb-property-editor></umb-property>',
     scope: {
       setFiles: '&',
       property: '=',
@@ -9,14 +11,15 @@ export default function EmbeddedContentPropertyDirective (fileManager) {
     },
 
     link (scope) {
-      const alias = scope.property.alias
-      const id = scope.property.id
+      scope.editProperty = angular.copy(scope.property)
+      const alias = scope.editProperty.alias
+      const id = scope.editProperty.id
 
       // return the original alias and id when the property is serialized
-      scope.property.toJSON = function () { return Object.assign({}, this, { alias: alias, id: id }) }
+      scope.editProperty.toJSON = function () { return Object.assign({}, this, { alias: alias, id: id }) }
 
       // make sure the property alias and id is unique
-      scope.property.alias = scope.property.id = `item-${scope.embeddedContentItem.key}-${alias}`
+      scope.editProperty.alias = scope.editProperty.id = `item-${scope.embeddedContentItem.key}-${alias}`
 
       scope.ready = true
       scope.$on('filesSelected', (e, args) => {
@@ -25,6 +28,34 @@ export default function EmbeddedContentPropertyDirective (fileManager) {
         fileManager.setFiles(scope.property.alias, [])
         scope.setFiles({ files: filesArray })
       })
+
+      scope.$on('formSubmitting', () => {
+        scope.property.value = scope.editProperty.value
+      })
+
+      scope.$watch('property.value', (newVal, oldVal) => {
+        if (newVal !== oldVal) {
+          scope.editProperty.value = newVal
+        }
+      })
+
+      // scope.$on('ecValueSync', (e, { items }) => {
+      //   if (items) {
+      //     items.forEach(item => {
+      //       if (item.key === scope.embeddedContentItem.key) {
+      //         [].concat(...item.tabs.map(t => t.properties))
+      //           .forEach(p => {
+      //             const propertyAlias = `item-${item.key}-${p.alias}`
+      //             if (propertyAlias === scope.property.alias) {
+      //               scope.property.value = p.value
+      //             }
+      //           })
+      //       }
+      //     })
+      //   }
+      //   console.log(scope.editProperty.value)
+      //   console.log(scope.property.value)
+      // })
     }
   }
 }
